cf3d4e4071bf5aee0df4278889829ed46bb8930f
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-09-10 10:44:26 -0300

    Planteo ejercicio 7

---
+++ js/Ejercicio07.js
@@ -0,0 +1,15 @@
+/*7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+
+Dificultad:  🟢🟡*/
+
+const topeIngresado = parseInt(
+  prompt("Ingrese un numero (max 50) para formar la piramide")
+);
